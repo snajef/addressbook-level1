2f41acde0fffd8e6db6a3fde1a037f78d535d28b
Darien Chong <e0176647@u.nus.edu>
2018-08-22 16:24:26 +0800

    Fixed a typo in src/seedu/addressbook/AddressBook.java (AddessBook -> AddressBook)
    Changed the internal representation of a person to use a Map instead of a String.
    Removed the variable PERSON_DATA_COUNT.
    Added a new private enum PersonProperty to function as the keys for the person Map representation.
    
    Added a new test to test/input.txt
    Added a new test outcome to test/expected.txt
    
    Formatted tests/runtests.bat for ease of reading.

--- test/runtests.bat
+++ test/runtests.bat
@@ -13,16 +13,21 @@ REM no error here, errorlevel == 0
 
 REM (invalid) no parent directory, invalid filename with no extension
 java -classpath ..\bin seedu.addressbook.AddressBook " " < NUL > actual.txt
+
 REM (invalid) invalid parent directory that does not exist, valid filename
 java -classpath ..\bin seedu.addressbook.AddressBook "directoryThatDoesNotExist/valid.filename" < NUL >> actual.txt
+
 REM (invalid) no parent directory, invalid filename with dot on first character
 java -classpath ..\bin seedu.addressbook.AddressBook ".noFilename" < NUL >> actual.txt
+
 REM (invalid) valid parent directory, non regular file
 if not exist data\notRegularFile.txt mkdir data\notRegularFile.txt
 java -classpath ..\bin seedu.addressbook.AddressBook "data/notRegularFile.txt" < NUL >> actual.txt
+
 REM (valid) valid parent directory, valid filename with extension.
 copy /y NUL data\valid.filename
 java -classpath ..\bin seedu.addressbook.AddressBook "data/valid.filename" < exitinput.txt >> actual.txt
+
 REM run the program, feed commands from input.txt file and redirect the output to the actual.txt
 java -classpath ..\bin seedu.addressbook.AddressBook < input.txt >> actual.txt
 
